38bcbd2470eaa33fbc7367243bcd2007b14dec41
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2017-09-15 19:48:57 +0200

    Add filler

--- lib/chimparse.rb
+++ lib/chimparse.rb
@@ -1,5 +1,58 @@
 require "chimparse/version"
+require "cgi"
 
 module Chimparse
-  # Your code goes here...
+  def self.fill(string, text_vars, html_vars)
+    vars_as_list = merge_vars_as_list(text_vars, html_vars)
+    content = vars_as_list.reduce(string) do |content, var|
+      content.gsub(regexp_for(var["name"]), var["content"])
+    end
+    conditionals(content, vars_as_list)
+  end
+
+  def self.merge_vars_as_list(text_vars, html_vars)
+    text_vars_as_list = text_vars.map do |k, v|
+      { "name" => k, "content" => CGI::escapeHTML(v.to_s) }
+    end
+    html_vars_as_list = html_vars.map do |k, v|
+      { "name" => k, "content" => v.to_s }
+    end
+    text_vars_as_list + html_vars_as_list
+  end
+
+  def self.conditionals(content, vars_as_list)
+    regexp = /\*\|IF:(\w+)\|\*(.*?)\*\|END:IF\|\*/mi
+    matches = regexp.match(content)
+    return content unless matches
+    array = else_if_branches(matches[0])
+    other_value = else_branch(matches[0]) if has_else?(matches[0])
+    new_content = content.sub(regexp, first_true(Hash[*array], vars_as_list) || other_value.to_s)
+    conditionals(new_content, vars_as_list)
+  end
+
+  def self.has_else?(string)
+    string.match(/\*\|ELSE:\|\*/mi)
+  end
+
+  def self.else_if_branches(string)
+    end_array = has_else?(string) ? -3 : -1
+    string.split(/\*\|IF:(.*?)\|\*|\*\|ELSEIF:(.*?)\|\*|\*\|(ELSE:|END:IF)\|\*/mi)[1...end_array]
+  end
+
+  def self.else_branch(string)
+    string.split(/\*\|ELSE:\|\*(.*?)\*\|END:IF\|\*/mi)[1]
+  end
+
+  def self.first_true(hash, vars_as_list)
+    pair = hash.find { |k, _| content_for(k, vars_as_list) }
+    pair && pair[1]
+  end
+
+  def self.regexp_for(key)
+    /\*\|(?:HTML:)?(#{key})\|\*/i
+  end
+
+  def self.content_for(key, vars_as_list)
+    vars_as_list.find { |v| v["name"].match(/\b#{key}\b/i) }&.dig("content")
+  end
 end
--- spec/chimparse_spec.rb
+++ spec/chimparse_spec.rb
@@ -1,11 +1,159 @@
 require "spec_helper"
 
 RSpec.describe Chimparse do
-  it "has a version number" do
-    expect(Chimparse::VERSION).not_to be nil
-  end
+  context "Local implementation" do
+    let(:content) { Chimparse.fill(html_content, text_vars, html_vars) }
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    context "text variables" do
+      let(:html_content) { "<h2>*|THE_VAR|*</h2>" }
+      let(:text_vars) { { the_var: "Hello!" } }
+      let(:html_vars) { {} }
+
+      it "replaces the variable" do
+        expect(content).to eq("<h2>Hello!</h2>")
+      end
+    end
+
+    context "HTML variables" do
+      let(:html_content) { "<h2>*|HTML:THE_VAR|*</h2>" }
+      let(:text_vars) { {} }
+      let(:html_vars) { { the_var: "<a>A link</a>" } }
+
+      it "replaces an HTML variable" do
+        expect(content).to eq("<h2><a>A link</a></h2>")
+      end
+    end
+
+    context "conditionals" do
+      let(:text_vars) { { this_is_true: true } }
+      let(:html_vars) { {} }
+
+      context "IF TRUE -- ENDIF" do
+        let(:html_content) {
+          <<-EOF
+          *|IF:THIS_IS_TRUE|*
+          YES
+          *|END:IF|*
+          EOF
+        }
+        it "outputs the true branch" do
+          expect(content).to include("YES")
+        end
+      end
+
+      context "IF FALSE -- ENDIF" do
+        let(:html_content) {
+          <<-EOF
+          *|IF:THIS_IS_FALSE|*
+          NO
+          *|END:IF|*
+          EOF
+        }
+        it "outputs the true branch" do
+          expect(content).not_to include("NO")
+        end
+      end
+
+      context "multiple" do
+        after do
+          expect(content).to include("YES")
+          expect(content).not_to include("NO")
+        end
+
+        context "IF TRUE -- ELSE -- ENDIF" do
+          let(:html_content) {
+            <<-EOF
+            *|IF:THIS_IS_TRUE|*
+            YES
+            *|ELSE:|*
+            NO
+            *|END:IF|*
+            EOF
+          }
+          it "outputs the true branch" do
+          end
+        end
+
+        context "IF FALSE -- ELSE -- ENDIF" do
+          let(:html_content) {
+            <<-EOF
+            *|IF:THIS_IS_FALSE|*
+            NO
+            *|ELSE:|*
+            YES
+            *|END:IF|*
+            EOF
+          }
+          it "outputs the true branch" do
+          end
+        end
+
+        context "IF TRUE -- ELSEIF TRUE -- ELSE -- ENDIF" do
+          let(:html_content) {
+            <<-EOF
+            *|IF:THIS_IS_TRUE|*
+            YES
+            *|ELSEIF:THIS_IS_ALSO_TRUE|*
+            NO
+            *|ELSE:|*
+            NO
+            *|END:IF|*
+            EOF
+          }
+          it "outputs the true branch" do
+          end
+        end
+
+        context "IF TRUE -- ELSEIF FALSE -- ELSE -- ENDIF" do
+          let(:html_content) {
+            <<-EOF
+            *|IF:THIS_IS_TRUE|*
+            YES
+            *|ELSEIF:THIS_IS_FALSE|*
+            NO
+            *|ELSE:|*
+            NO
+            *|END:IF|*
+            EOF
+          }
+          it "outputs the true branch" do
+          end
+        end
+
+        context "IF FALSE -- ELSEIF TRUE -- ELSE -- ENDIF" do
+          let(:html_content) {
+            <<-EOF
+            *|IF:THIS_IS_FALSE|*
+            NO
+            *|ELSEIF:THIS_IS_TRUE|*
+            YES
+            *|ELSEIF:THIS_IS_ALSO_TRUE_BUT_DOESNT_MATTER|*
+            NO
+            *|ELSE:|*
+            NO
+            *|END:IF|*
+            EOF
+          }
+          it "outputs the true branch" do
+          end
+        end
+
+        context "IF FALSE -- ELSEIF FALSE -- ELSE -- ENDIF" do
+          let(:html_content) {
+            <<-EOF
+            *|IF:THIS_IS_FALSE|*
+            NO
+            *|ELSEIF:THIS_IS_FALSE|*
+            NO
+            *|ELSE:|*
+            YES
+            *|END:IF|*
+            EOF
+          }
+          it "outputs the true branch" do
+          end
+        end
+      end
+    end
   end
 end
